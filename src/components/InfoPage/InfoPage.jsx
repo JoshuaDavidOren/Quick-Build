49f4ba2104774c9849191096d07b0c4316206a58
Joshua Oren <joshuadavidoren@gmail.com>
2021-09-23 11:28:48 -0500

    updated site with good link examples

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is
 
+
 function InfoPage() {
 
   useEffect(() => {
@@ -17,17 +18,24 @@ function InfoPage() {
       <h1> H1 Info Page</h1>
       <section id='info'>
       <div class="exampleHeadings">
-  <h2> H2 Setting the ISO</h2>
+  <h2> H2 Headings</h2>
     <h3> H3 The effect of ISO on image quality</h3>
       <p>The higher the ISO, the greater your 
       ability to take photos in low light [...]</p>
     <h3> H3 High ISO cameras</h3>
       <p>Cameras with larger sensors are capable of 
       higher ISO values [...]</p>
-  <h2> H2 Choose an aperture</h2>
-    <h3> H3 The effect of aperture on depth of focus</h3>
-      <p>The larger the aperture, the narrower 
-      the plane of focus [...]</p>
+  <h2> H2 Links</h2>
+    <h3> Examples of good Links</h3>
+      <p>
+      <a href="https://dequeuniversity.com/contact/">Home Page</a>
+        This link is named to describe what it does (a screen reader will say "link, Home Page")
+      </p>
+      <p>
+        <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fthenounproject.com%2Fterm%2Fnew-window%2F&psig=AOvVaw2P5HOc5wBF0lfSXxYCT3Us&ust=1632500063660000&source=images&cd=vfe&ved=0CAwQjhxqFwoTCICwheW-lfMCFQAAAAAdAAAAABAD" target="_blank">Vector for opens new window image 
+    <img src="https://static.thenounproject.com/png/299638-200.png" alt="opens new window" width="16" height="16"/>
+  </a> This link opens a new window the image next to it not only provides a visual indication for the user, but also has alternative text that screen readers can relay to its users.
+  </p>
     <h3> H3 Vignetting</h3>
       <p>Vignetting occurs when a lens lets in less light 
       around the edges than in the center [...]</p>
